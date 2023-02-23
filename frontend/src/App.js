ecf497af8b2d5a7ae707c4092d61d85a87a3ff3b
Avneesh002 <Avneeshgrover8@gmail.com>
2023-02-23 22:09:21 +0530

    review section remaining

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
 
 import AllRoutes from "./Routes/AllRoutes";
-import SingleMovie from "./Pages/SingleMovie";
 
 function App() {
   return (
     <div className="App">
-      {/* <AllRoutes /> */}
-      <SingleMovie />
+      <AllRoutes />
     </div>
   );
 }
