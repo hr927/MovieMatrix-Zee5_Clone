178e0a248cb01ea2736380787ba4ff5b098bc6fd
rahulyadav826870 <rahulyadav826870@gmail.com>
2023-02-23 14:23:04 +0530

    Private Route

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../Components/Login";
+import PrivateRoute from "../Components/PrivateRoute";
 import Sighup from "../Components/Sighup";
 
 export default function AllRoutes() {
