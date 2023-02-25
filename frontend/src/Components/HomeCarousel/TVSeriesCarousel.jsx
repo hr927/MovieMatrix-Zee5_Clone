7fe27c4a04b68d5479bb326792f15e6dfd980e5f
Soumya0587 <ghoshalsoumalya96@gmail.com>
2023-02-25 21:23:44 +0530

    day-5

--- frontend/src/Components/HomeCarousel/TVSeriesCarousel.jsx
+++ frontend/src/Components/HomeCarousel/TVSeriesCarousel.jsx
@@ -1,7 +1,7 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
+import { FaPlay } from "react-icons/fa";
 import React from "react";
 import { getTagsMediaTypedata } from "../Home_mid_Section/api";
 import { Link, useNavigate } from "react-router-dom";
@@ -48,20 +48,21 @@ export default function TVSeriesCarousel() {
             {/* <Box position="absolute" top={0} left={0} bottom={0} right={0} bg="rgba(255, 255, 255, 0.5)" filter="blur(10px)" zIndex="-1"></Box> */}
             <Container height="600px" position="relative">
               <Stack
-                spacing={6}
+                spacing={2}
                 w={"full"}
                 maxW={"lg"}
                 position="absolute"
                 top="50%"
+                right="40%"
                 transform="translate(0, -50%)"
               >
-                <Button color={"black"}>Watch</Button>
+                
 
                 <Text fontSize={{ base: "2xl", md: "3xl", lg: "3xl" }}>
                   {item.title}
                 </Text>
                 <Link to={`../details/${item._id}`}>
-                  <Button color="grey">Watch</Button>
+                  <Button colorScheme='purple'>Details</Button>
                 </Link>
               </Stack>
             </Container>
--- frontend/src/Components/Home_mid_Section/api.jsx
+++ frontend/src/Components/Home_mid_Section/api.jsx
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 export const getdata = (query) =>{
-    return axios.get(`http://localhost:8080/media?tags=${query}`)
+    return axios.get(`https://bronze-salamander-cuff.cyclic.app/media?tags=${query}`)
 }
 export const getgenredata = (query) =>{
-    return axios.get(`http://localhost:8080/media?genre=${query}`)
+    return axios.get(`https://bronze-salamander-cuff.cyclic.app/media?genre=${query}`)
 }
 export const getTagsMediaTypedata = (query1,query2) =>{
-    return axios.get(`http://localhost:8080/media?tags=${query1}&mediaType=${query2}`)
+    return axios.get(`https://bronze-salamander-cuff.cyclic.app/media?tags=${query1}&mediaType=${query2}`)
 }
 export const getgenreMediaTypedata = (query1,query2) =>{
-    return axios.get(`http://localhost:8080/media?genre=${query1}&mediaType=${query2}`)
+    return axios.get(`https://bronze-salamander-cuff.cyclic.app/media?genre=${query1}&mediaType=${query2}`)
 }
